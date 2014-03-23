d7b7873755b625c2e360e16b4fe30f74f55ee021
joe pugh <joe@makeawesomesh.it>
2014-03-23 03:10:42 -0400

    added 2d output, TAB Lifegit status

--- posgen/patgen.py
+++ posgen/patgen.py
@@ -16,6 +16,7 @@ pstep = Pheight/64
 
 for x in range(10):
     for y in range(10):
+        xy.append([step*x-xoff,step*y-yoff])
         for z in range(72):
             t =[step*x-xoff,step*y-yoff,Pheight-(pstep*z)-zoff]
             points.append(t)
@@ -36,3 +37,7 @@ f = open('grid.txt','w')
 for x in points:
     f.write('%.2f,%.2f,%.2f\n' % (x[0], x[1], x[2]))
 f.close()
+
+f = open('2dgrid.txt','w')
+for i in xy:
+    f.write('%.2f,%.2f\n' % (i[0], i[1]))
