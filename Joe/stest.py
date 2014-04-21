4862a0d30400ce1b130b4361b1f20b74fc782316
joe pugh <joe@makeawesomesh.it>
2014-04-21 05:26:57 -0400

    added wrong stest before, fixing

--- Joe/stest.py
+++ Joe/stest.py
@@ -1,25 +1,48 @@
+import cb
+import shapes
+import opc
+import time
 import numpy as np
-import scipy.spatial as sp
 
-class sphere():
-    
-    def __init__(self, bpos, alpha, size = 1):
-        self.pos = bpos
-        self.alpha = alpha
-        self.size = size
+IP_PORT = '127.0.0.1:7890'
 
-    #def calcdist(self, pos):
-        #return sp.distance.cdist(points,ballpos)
-    def calcdist(self, pos):
-        global points
-        print np.clip(self.size-sp.distance.cdist(points,self.pos))
+client = opc.Client(IP_PORT)
+if client.can_connect():
+    print '    connected to %s' % IP_PORT
+else:
+    # can't connect, but keep running in case the server appears later
+    print '    WARNING: could not connect to %s' % IP_PORT
 
-    #def moveball(ball):
-        #global ballspeed
-        #if (ballpos[0,1] < -2.286) or (ballpos[0,1] > 2.286):
-            #ballspeed = ballspeed * -1
-        #b= float(ballpos[0,1]+ballspeed)
-        #ballpos[0,1]= b
+n_pixels = 7200
+##*************read grid******************##
+f = open(grid.txt,'r')
+xyz = []
+for i in f:
+    x= float(i.split(',')[0].strip())
+    y= float(i.split(',')[1].strip())
+    z= float(i.split(',')[1].strip())
+    self.xyz.append([x,y,z])
+points = np.asarry(xyz)
 
-    #def flipandinvert(n):
-        #return (n-1)*-255
+#c = cb.background("2dgrid.txt",2.4384,72)
+c = shapes.sphere(0,[1,1,1])
+comp = cb.compositor()
+pixels = []
+start_time = time.time()
+while 0:
+    pixels = []
+    current_time = start_time - time.time()
+    c.surfacewave(current_time)
+    #c.radwave(current_time)
+    p = c.pixlist()
+    for pi in p:
+        pixels.append([pi[0]*256,pi[1]*256,pi[2]*256])
+    client.put_pixels(pixels, channel=0)
+
+while 1:
+    pixels = []
+    current_time = start_time - time.time()
+    c.rainbow(current_time)
+    p = c.pixlist()#works as an update right now
+    print comp.flatten([c])
+    client.put_pixels(comp.flatten([c]).tolist(), channel=0)
