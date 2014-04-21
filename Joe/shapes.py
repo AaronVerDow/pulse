ad4dc356a8bf5690716580f89e548d7a66f3e0b7
joe pugh <joe@makeawesomesh.it>
2014-04-21 05:11:54 -0400

    some small work on the shapes file

--- Joe/shapes.py
+++ Joe/shapes.py
@@ -3,15 +3,23 @@ import scipy.spatial as sp
 
 class sphere():
     
-    def calcdist():
-        return sp.distance.cdist(points,ballpos)
+    def __init__(self, bpos, alpha, size = 1):
+        self.pos = bpos
+        self.alpha = alpha
+        self.size = size
 
-    def moveball(ball):
-        global ballspeed
-        if (ballpos[0,1] < -2.286) or (ballpos[0,1] > 2.286):
-            ballspeed = ballspeed * -1
-        b= float(ballpos[0,1]+ballspeed)
-        ballpos[0,1]= b
+    #def calcdist(self, pos):
+        #return sp.distance.cdist(points,ballpos)
+    def calcdist(self, pos):
+        global points
+        print np.clip(self.size-sp.distance.cdist(points,self.pos))
 
-    def flipandinvert(n):
-        return (n-1)*-255
+    #def moveball(ball):
+        #global ballspeed
+        #if (ballpos[0,1] < -2.286) or (ballpos[0,1] > 2.286):
+            #ballspeed = ballspeed * -1
+        #b= float(ballpos[0,1]+ballspeed)
+        #ballpos[0,1]= b
+
+    #def flipandinvert(n):
+        #return (n-1)*-255
