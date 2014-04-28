abc6c6ab40c356ed40cafeb14ce7b6ec7a2f12a7
joe pugh <joe@makeawesomesh.it>
2014-04-28 05:32:57 -0400

    clean up

--- patterns/vox_shapes.py
+++ patterns/vox_shapes.py
@@ -5,12 +5,14 @@ import vox_color
 #spheres for making ... spheres
 class sphere():
     pixlist = []
+    alphamask = []
     #take center position of sphere, color in rgb 0-1 or a color object,
     #size, mode (fill or fade), and alpha (0-1 as a scaler or rgb list)
     def __init__(self,spos, color, points, size = 1, mode = 'fade', alpha = 1):
         self.pos = np.transpose(spos)
         self.size = size
         self.points = points
+
         #handling veriable in the way color may be passed
         if type(color) == vox_color.color: #if color is of color object we are good
             print "that is a god damn color"
@@ -19,6 +21,7 @@ class sphere():
             self.color = vox_color.color(color)
         else:#else make it just black, you should not have fucked up if you wanted color
             self.color = vox_color.color('black')
+
         #handling ways that alpha can be passed
         if type(alpha) == int:
             self.alpha = [alpha,alpha,alpha]
