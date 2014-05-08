a5acc77ab2df011ed59f0e2416af1322f1a09715
joe pugh <joe@makeawesomesh.it>
2014-05-08 05:58:41 -0400

    added some default patterns to surface

--- patterns/vox_points.py
+++ patterns/vox_points.py
@@ -3,22 +3,27 @@ import scipy.spatial as sp
 import vox_color
 from vox_shapes import layer
 import time
+from math import *
 
 #base class for holding array of points
 class pointholder():
     
     #list of points around the cube
-    points = []
-    pointsindex = []
+#    points = []
+#    pointsindex = []
     
     #values of pixels for full cube to be used by compiler
-    pixlist = []
-    alphamask = []
+#    pixlist = []
+#    alphamask = []
+    
+    #store modifications to run over the points
+#    mod = []
     
     #time
     stime = 0
     ctime = 0
     
+    #add a point
     def add(self,**newpoint):
         self.points.append(newpoint)
     
@@ -26,6 +31,22 @@ class pointholder():
     def remove(self,num):
         points.pop(num)
 
+    #this add a new mod to the stack.  no checks on what you put in
+    #if you do it wrong you will just crash everything so check you 
+    #target ie.
+    #'modname':wavex.....
+    def addmod(self,**func):
+        self.mod.append(func)
+    
+    def addxwave(self):
+        self.mod.append({'modname':self.xwave})
+        
+    def addywave(self):
+        self.mod.append({'modname':self.ywave})
+        
+    def addxywave(self):
+         self.mod.append({'modname':self.xywave})
+            
     #def update pixlist
     def updatepix(self):
         #make a new array the size of the final pixel list
@@ -36,14 +57,8 @@ class pointholder():
             for rp in range(len(rendstrip)):
                 if rendstrip[rp] != 0:
                     pixnumber = p['sid']*self.gridinfo['zcount']+rp
-                    print "p['sid] "+str(p['sid']),
-                    print " self.gridinfo['zcount'] "+str(self.gridinfo['zcount']),
-                    print " rp "+str(rp)
                     px[pixnumber]=rendstrip[rp]
-        print np.shape(px)
-        #self.alphamask = px*np.array(self.alpha).reshape(3,1)
         self.alphamask = px*self.alpha
-        print np.shape(self.alphamask)
         self.pixlist = self.alphamask*self.color.c
 
     #calculate distance on a 1d plain
@@ -58,6 +73,8 @@ class pointholder():
     
     #update pixlist
     def update(self):
+        for m in self.mod:
+            m['modname']()
         self.updatepix()
         
         
@@ -65,6 +82,13 @@ class pointholder():
 #surface of points
 class surface(pointholder,layer):
     def __init__(self,xy,color,size,z,alpha,**griddata):
+        #####################not sure why I have to do this will fix later
+        self.mod = []
+        self.pixlist = []
+        self.alphamask = []
+        self.points = []
+        self.pointsindex = []
+        #######################
         for l in range(len(xy)):
             #self.add(l,xy[l][0],xy[l][1],z,size)
             self.add(**{'sid':l,'x': xy[l][0],'y': xy[l][1],'z':z,'size':size})
@@ -78,17 +102,24 @@ class surface(pointholder,layer):
     #changing the starting z val of all points in the surface
     def changez(self,z):
         for l in self.points:
-#            l[3]=z
             l['z']=z
     
     #changing the size of all points in the surface
     def changesize(self,size):
         for l in self.points:
-#            l[4]=size
             l['size'] = size
     
-    def swave(self,):
-        pass
+    def xwave(self):
+        for p in self.points:
+            p['z']=sin(p['x']+(time.time()-self.stime))
+            
+    def ywave(self):
+        for p in self.points:
+            p['z']=sin(p['y']+(time.time()-self.stime))
+            
+    def xywave(self):
+        for p in self.points:
+            p['z']=(sin(p['y']+(time.time()-self.stime))+sin(p['x']+(time.time()-self.stime)))/2
         
 class pointgroup(pointholder,layer):
     
