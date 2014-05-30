42266d0bb1b800ba26e239d64aadceaa0310d50b
averdow <averdow@pulse>
2014-05-30 01:15:39 -0400

    updated fcserver.config

--- make_server_config.sh
+++ make_server_config.sh
@@ -11,14 +11,27 @@ cat << EOF
     "devices": [
 EOF
 SERIALS=`cat serial_nos | awk '{print $2}'`
+NUMBER=0
+STRIP_NO=10
+PIXEL_NO=0
 for s in $SERIALS
 do
-cat << EOF
+    cat << EOF
         {
             "type": "fadecandy",
             "serial": "$s",
             "map": [
-                [ 0, 0, 0, 512 ]
+EOF
+    for i in {0..7}
+    do
+        let $((PIXEL_LOCAL=i*64))
+        cat << EOF
+                [ 0, $PIXEL_NO, $PIXEL_LOCAL, 64 ] //$STRIP_NO
+EOF
+        let $((STRIP_NO=STRIP_NO+1))
+        let $((PIXEL_NO=PIXEL_NO+64))
+    done
+    cat << EOF
             ]
         },
 EOF
