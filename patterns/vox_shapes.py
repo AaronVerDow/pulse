a53a740ab63bdbe1091117c8f09c8bf9ee0e8d05
joe pugh <joe@makeawesomesh.it>
2014-06-15 02:20:34 -0400

    added new modes for the sphere

--- patterns/vox_shapes.py
+++ patterns/vox_shapes.py
@@ -1,27 +1,9 @@
 import numpy as np
 import scipy.spatial as sp
 import vox_color
+from vox import layer
 import griddata as gd
-
-#base layer to hold common actions like dynamicly taking color layers   
-class layer():
-    def colorhandle(self,color):
-        #handling veriable in the way color may be passed
-        if type(color) == vox_color.color: #if color is of color object we are good
-            print "that is a god damn color"
-            return color
-        elif type(color) == list:#if color is a list make a color object out of it
-            return vox_color.color(color)
-        else:#else make it just black, you should not have fucked up if you wanted color
-            return vox_color.color('black')
-
-    def alphahandle(self,alpha):
-        #handling ways that alpha can be passed
-        if type(alpha) == int:
-#            return [alpha,alpha,alpha]
-            return alpha
-        else:
-            return alpha
+import time
 
 #spheres for making ... spheres
 class sphere(layer):
@@ -29,10 +11,15 @@ class sphere(layer):
     alphamask = []
     #take center position of sphere, color in rgb 0-1 or a color object,
     #size, mode (fill or fade), and alpha (0-1 as a scaler or rgb list)
-    def __init__(self,spos, color, size = 1, mode = 'fade', alpha = 1):
+    def __init__(self,spos, color, size = 1, mode = 'fade', alpha = 1, maxspeed = .1, ratio = 1.0):
         self.pos = np.transpose(spos)
         self.size = size
         self.points = gd.grid3d
+        self.maxspeed = maxspeed
+        self.ratio = ratio
+        self.target = self.pos
+        self.lastrun = time.time()
+        self.mode = mode
         
         #pass color to colorhandle in layer class to set it up.
         self.color = self.colorhandle(color)
@@ -42,13 +29,25 @@ class sphere(layer):
     def calcdist(self, pos):
         #calculates the distance from center for each pixel.  clips so
         #we don't have any values out of the range 0-1
-        return np.clip(self.size-sp.distance.cdist(self.points,pos),0,1)
+        return np.clip((self.size-sp.distance.cdist(self.points,pos)),0,1)
 
 
     def update(self):
+        self.movetowards()
         dis = self.calcdist(self.pos)
-        self.pixlist = (self.alpha*dis)*self.color.c
-        self.alphamask = (self.alpha*dis)
-    
-    def maskgen(self):
-        pass
+        if self.mode == 'fade':
+            self.pixlist = (self.alpha*dis)*self.color.c
+            self.alphamask = (self.alpha*dis)
+        elif self.mode == 'fill':
+            m = dis>0
+            self.pixlist = (self.alpha*m)*self.color.c
+            self.alphamask = (self.alpha*m)
+        elif self.mode == 'bubble':
+            m = dis>0
+            self.pixlist = (1-dis)*m*self.alpha*self.color.c
+            self.alphamask = (1-dis)*m*self.alpha
+            
+    def movetowards(self):
+        ctime = time.time()-self.lastrun
+        self.pos = self.pos+np.clip(((self.target-self.pos)*self.ratio*ctime),-self.maxspeed,self.maxspeed)
+        self.lastrun = time.time()
