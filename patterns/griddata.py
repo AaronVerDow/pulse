05273ed68dc0214a177f5fcf61241e8dd2d19f45
averdow <averdow@u2c413814fbd05122601b.ant.amazon.com>
2014-06-18 01:22:02 -0400

    added box object

--- patterns/griddata.py
+++ patterns/griddata.py
@@ -1,13 +1,32 @@
 import numpy as np
 
+x_steps = 10
+y_steps = 10
+z_steps = 64
+
 ##*************read grid******************##
 f = open('grid.txt','r')
 xyz = []
+exploded = np.zeros((x_steps,y_steps,z_steps,4))
+##c = count
+xc = 0
+yc = 0
+zc = 0
 for i in f:
     x= float(i.split(',')[0].strip())
     y= float(i.split(',')[1].strip())
     z= float(i.split(',')[2].strip())
     xyz.append([x,y,z])
+
+    if zc == z_steps:
+        zc = 0
+        yc = yc + 1
+    if yc == y_steps:
+        yc = 0
+        xc = xc + 1
+    exploded[xc][yc][zc] = [x,y,z,0]
+    zc = zc + 1
+    
 grid3d = np.array(xyz)
 f.close()
 ##****************************************##
--- patterns/vox_shapes.py
+++ patterns/vox_shapes.py
@@ -5,6 +5,74 @@ from vox import layer
 import griddata as gd
 import time
 
+class box(layer):
+    pixlist = []
+    alphamask = []
+    #take center position of sphere, color in rgb 0-1 or a color object,
+    #size, mode (fill or fade), and alpha (0-1 as a scaler or rgb list)
+    def __init__(
+        self,
+        spos,
+        color,
+        x = 1,
+        y = 1,
+        z = 1,
+        alpha = 1,
+        ratio = 1.0):
+
+        self.pos = np.transpose(spos)
+        self.x = x
+        self.y = y
+        self.z = z
+        #there has got to be a better way to do this
+        self.points = gd.grid3d
+        self.target = self.pos
+        self.lastrun = time.time()
+        #pass color to colorhandle in layer class to set it up.
+        self.color = self.colorhandle(color)
+        #pass alpha to alpha handle in layer class to set it up.
+        self.alpha = self.alphahandle(alpha)
+
+    def update(self):
+ 
+        points = gd.exploded
+
+        x_min = 0
+        x_max = 9
+        y_min = 0
+        y_max = 9
+        z_min = 0
+        z_max = 63
+        listed_points = []
+
+        while points[0][0][z_min][2] > self.pos[0][2] + self.z/2 and z_min < z_max:
+            z_min = z_min + 1
+        while points[0][0][z_max][2] < self.pos[0][2] - self.z/2 and z_min < z_max:
+            z_max = z_max - 1
+        while points[x_min][0][0][0] < self.pos[0][0] - self.x/2 and x_min < x_max:
+            x_min = x_min + 1
+        while points[x_max][0][0][0] > self.pos[0][0] + self.x/2 and x_min < x_max:
+            x_max = x_max - 1
+        while points[0][y_min][0][1] < self.pos[0][1] - self.y/2 and y_min < y_max:
+            y_min = y_min + 1
+        while points[0][y_max][0][1] > self.pos[0][1] + self.y/2 and y_min < y_max:
+            y_max = y_max - 1
+
+        for x in range(x_min, x_max):
+            for y in range(y_min, y_max):
+                for z in range(z_min, z_max):
+                    points[x][y][z][3] = 1
+
+        for x in range(0,len(points)):
+            for y in range(0,len(points[0])):
+                for z in range(0,len(points[0][0])):
+                    listed_points.append([points[x][y][z][3]])
+
+        self.alphamask = (self.alpha*listed_points)
+        self.pixlist = listed_points*self.color.c
+        #self.pixlist = listed_points*self.alpha*self.color.c
+
+
 #spheres for making ... spheres
 class sphere(layer):
     pixlist = []
